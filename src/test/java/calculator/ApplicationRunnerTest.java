df07b20686547bb4605cf5e0ad97243fab6eb31b
minuuu0 <rlamw2000@naver.com>
2025-10-17 09:50:32 +0900

    test(ApplicationRunnerTest): 하드코딩된 출력 문자열을 expectedOutput 메서드로 추출 및 잘못된 빈 문자열 테스트 수정

--- src/test/java/calculator/ApplicationRunnerTest.java
+++ src/test/java/calculator/ApplicationRunnerTest.java
@@ -9,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationRunnerTest extends NsTest {
+    private static final String INPUT_PROMPT = "덧셈할 문자열을 입력해 주세요.";
+
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
@@ -28,8 +30,8 @@ class ApplicationRunnerTest extends NsTest {
     @Test
     void 빈_문자열을_입력할_경우_0을_반환() {
         assertSimpleTest(() -> {
-            run("");
-            assertThat(output()).isEqualTo("덧셈할 문자열을 입력해 주세요." + System.lineSeparator() + "결과 : 0");
+            run("\n");
+            assertThat(output()).isEqualTo(expectedOutput(0));
         });
     }
 
@@ -37,9 +39,14 @@ class ApplicationRunnerTest extends NsTest {
     void 숫자_하나를_입력할_경우_해당_숫자를_그대로_반환() {
         assertSimpleTest(() -> {
             run("1");
-            assertThat(output()).isEqualTo("덧셈할 문자열을 입력해 주세요." + System.lineSeparator() + "결과 : 1");
+            assertThat(output()).isEqualTo(expectedOutput(1));
         });
     }
+
+    private String expectedOutput(int result) {
+        return INPUT_PROMPT + System.lineSeparator() + "결과 : " + result;
+    }
+
 
     @Override
     public void runMain() {
